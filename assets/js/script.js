ceb98790cdb46311b9cd81c46be9080e42f1915f
Brandon <brandonperez@brandons-mbp.lan>
2022-06-20 00:51:02 -0400

    fully working application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 //hardcoded values for testing. Api Query Parameteres
 var APIkey = '923d9e379d8c5e5c3deb64d1aca43984'
-var lat = '28.1983'
-var lon = '-82.3305'
 var part = 'alerts,minutely'
 
 //setup for dates
@@ -9,36 +7,130 @@ const date = new Date()
 var day = date.getDate()
 var month = date.getMonth() + 1;
 var year = date.getFullYear();
+//formats date
 const currentDate = `(${month}/${day}/${year})`
 
-//fetch function
-var apiCall = function () {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}&units=imperial`
-    console.log(apiUrl);
+//array for local storage
+var citiesArray = [];
+
+//retrieving from local storage
+var retrieveLocalStorage = function () {
+    var recentCities = document.querySelector('#recent-searches');
+    var LocalStorageRetrieve = JSON.parse(localStorage.getItem('city'));
+
+    if (LocalStorageRetrieve) {
+        //clears out old searches to prevent duplicate saving
+        recentCities.textContent = ''
+        //iterates through local storage and creates buttons
+        for (let i = 0; i < LocalStorageRetrieve.length; i++) {
+            //calls function to make actual button, passing in argument for text content
+            var makeButton = buttonCreation(LocalStorageRetrieve[i].city);
+            //sets attribute for styling for button
+            makeButton.setAttribute('class', 'btn btn-secondary m-2 city-button')
+            //appends to the div
+            recentCities.appendChild(makeButton);
+        }
+        //if there is nothing in local storage, run this
+    } else {
+        recentCities.innerHTML = "Your recent searches will show here!"
+    }
+}
+
+//function to create a button. Called multiple times in retrieveLocalStorage based on local storage length
+var buttonCreation = function (content) {
+    var citybtn = document.createElement('button');
+    citybtn.textContent = content;
+    //onclick, this function will run that calls another function that grabs the value of the button
+    citybtn.onclick = function () { getButtonValue(this); };
+    //returns the button
+    return citybtn
+};
+
+//grabs the value of the button and calls the location function.
+var getButtonValue = function (value) {
+    var buttonValue = value.innerHTML;
+    locationApiCall(buttonValue);
+}
+// //runs one time if local storage is present so object doesnt keep getting pushed
+var loadLocalStorage = function () {
+    console.log("i should only run once")
+    //have to set up if statement or null object will be pushed
+    if (localStorage.getItem('city')) {
+        var LocalStorageRetrieve = JSON.parse(localStorage.getItem('city'));
+        for (let i = 0; i < LocalStorageRetrieve.length; i++) {
+            var cityCaptureObj = {
+                city: LocalStorageRetrieve[i].city
+            };
+            console.log("im pushing to the array")
+            citiesArray.push(cityCaptureObj);
+        }
+        retrieveLocalStorage();
+    } else {
+        console.log('nothing in local storage')
+    }
+}
+
+//captures search field for city name and passes into to next fetch function
+var locationCatch = function (event) {
+    event.preventDefault();
+
+    var cityCapture = document.querySelector('#city-capture').value.trim();
+
+    var cityCaptureObj = { city: cityCapture };
+
+    console.log("im pushing to the array")
+    citiesArray.push(cityCaptureObj);
+
+    localStorage.setItem('city', JSON.stringify(citiesArray));
+
+    locationApiCall(cityCapture)
+
+    //calls function after every search to save recent search
+    retrieveLocalStorage();
+}
+
+//fetch function based off city name
+var locationApiCall = function (cityname) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(function (response) {
-            console.log(response);
-            generateCurrent(response);
+            var lat = response.coord.lat;
+            var lon = response.coord.lon
+            apiCall(lat, lon, cityname)
+        })
+}
+
+//fetch function based of lat lon and other query parameters
+var apiCall = function (lat, lon, cityname) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}&units=imperial`
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(function (response) {
+            generateCurrent(response, cityname);
             getFiveDays(response);
         });
 };
 
 //generates current weather conditions
-var generateCurrent = function (weather) {
+var generateCurrent = function (weather, cityname) {
     //temp, wind, humidity, uv index
     var current = weather.current;
 
     //hard coded location
     var headingEl = document.querySelector('#location');
-    headingEl.textContent = "Wesley Chapel " + currentDate
+    headingEl.textContent = cityname
 
     //current variables
     var temp = current.temp;
     var wind = current.wind_speed;
     var humidity = current.humidity;
     var uvIndex = current.uvi;
+
+    var uvIndexLiEl = document.createElement('li')
+    uvIndexLiEl.textContent = `UV Index: ${uvIndex}`
 
     //selects ul 
     var currentConditions = document.querySelector('#current-conditions')
@@ -47,8 +139,9 @@ var generateCurrent = function (weather) {
     <li> Temperature: ${temp} \u00B0F  </li>
     <li> Wind: ${wind} MPH </li>
     <li> Humidity: ${humidity} %</li>
-    <li> Uv Index: ${uvIndex} </li>
     `
+    currentConditions.appendChild(uvIndexLiEl)
+    checkUVIndex(uvIndex, uvIndexLiEl)
 };
 
 //generates the next five day forecast
@@ -61,8 +154,8 @@ var getFiveDays = function (weather) {
     //creates five div cards to contain weather information
     for (let i = 0; i < daily.length - 3; i++) {
         //formating for the next five dates
-        var futureDay = date.getDate()+i+1;
-    
+        var futureDay = date.getDate() + i + 1;
+
         var dayDivEl = document.createElement('div');
         dayDivEl.setAttribute('class', 'card col bg-info mx-2 border border-dark');
 
@@ -95,6 +188,21 @@ var getFiveDays = function (weather) {
     };
 };
 
-apiCall()
+//changes backgroun color based on uvIndex
+var checkUVIndex = function (uvindex, uvIndexLiEl) {
+    if (uvindex < 3) {
+        uvIndexLiEl.setAttribute('class', 'bg-success')
+    } else if (3 < uvindex < 6) {
+        uvIndexLiEl.setAttribute('class', 'bg-warning')
+    } else {
+        uvIndexLiEl.setAttribute('class', 'bg-danger')
+    }
+    return uvIndexLiEl
+}
+
+loadLocalStorage();
+
+
+document.querySelector('#city-search').addEventListener('submit', locationCatch)
 
 
